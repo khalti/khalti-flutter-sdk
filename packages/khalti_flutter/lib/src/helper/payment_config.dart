2b30e0f3da2d7708a8915d82137555c072d5856b
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-22 10:20:15 +0545

    :sparkles: added `PaymentConfig` and its provider

---
+++ packages/khalti_flutter/lib/src/helper/payment_config.dart
@@ -0,0 +1,34 @@
+class PaymentConfig {
+  PaymentConfig({
+    required this.amount,
+    required this.productIdentity,
+    required this.productName,
+    this.productUrl,
+    this.additionalData,
+  });
+
+  final int amount;
+  final String productIdentity;
+  final String productName;
+  final String? productUrl;
+  final Map<String, Object>? additionalData;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PaymentConfig &&
+          runtimeType == other.runtimeType &&
+          amount == other.amount &&
+          productIdentity == other.productIdentity &&
+          productName == other.productName &&
+          productUrl == other.productUrl &&
+          additionalData == other.additionalData;
+
+  @override
+  int get hashCode =>
+      amount.hashCode ^
+      productIdentity.hashCode ^
+      productName.hashCode ^
+      productUrl.hashCode ^
+      additionalData.hashCode;
+}
---
+++ packages/khalti_flutter/lib/src/helper/payment_config_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:khalti_flutter/src/helper/payment_config.dart';
+
+class PaymentConfigScope extends InheritedWidget {
+  PaymentConfigScope({
+    Key? key,
+    required this.config,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  final PaymentConfig config;
+
+  static PaymentConfig of(BuildContext context) {
+    final PaymentConfigScope? configScope =
+        context.dependOnInheritedWidgetOfExactType();
+
+    assert(
+      configScope != null,
+      'No PaymentConfigScope found in the widget tree.',
+    );
+    return configScope!.config;
+  }
+
+  @override
+  bool updateShouldNotify(PaymentConfigScope old) => old.config != config;
+}
