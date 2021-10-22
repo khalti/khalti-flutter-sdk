f9f6beecd9ae08d6acc482bf0a2a8e7914a06b8e
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-22 10:18:29 +0545

    :sparkles: added example flutter app

---
+++ packages/khalti_flutter/example/README.md
@@ -0,0 +1,1 @@
+# Khalti Payment Gateway
---
+++ packages/khalti_flutter/example/android/app/src/main/kotlin/com/khalti/khalti_flutter_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.khalti.khalti_flutter_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
