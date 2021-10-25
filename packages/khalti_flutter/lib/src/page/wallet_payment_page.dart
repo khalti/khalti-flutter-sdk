1124a60700522027a74d9feb471ed64c4a965af0
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-25 12:18:32 +0545

    :sparkles: added success dialog

--- packages/khalti_flutter/lib/src/page/wallet_payment_page.dart
+++ packages/khalti_flutter/lib/src/page/wallet_payment_page.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:khalti/khalti.dart';
+import 'package:khalti_flutter/src/helper/payment_config_provider.dart';
+import 'package:khalti_flutter/src/widget/dialogs.dart';
 import 'package:khalti_flutter/src/widget/fields.dart';
 import 'package:khalti_flutter/src/widget/image.dart';
 import 'package:khalti_flutter/src/widget/pay_button.dart';
 
-class WalletPaymentPage extends StatelessWidget {
+class WalletPaymentPage extends StatefulWidget {
   const WalletPaymentPage({Key? key}) : super(key: key);
 
   @override
+  State<WalletPaymentPage> createState() => _WalletPaymentPageState();
+}
+
+class _WalletPaymentPageState extends State<WalletPaymentPage>
+    with AutomaticKeepAliveClientMixin {
+  String? _mobile, _mPin;
+
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
+
+    final config = PaymentConfigScope.of(context);
+
     return ListView(
       padding: EdgeInsets.all(16),
       children: [
@@ -16,16 +31,48 @@ class WalletPaymentPage extends StatelessWidget {
           child: KhaltiImage.asset(asset: 'logo/khalti.svg', height: 72),
         ),
         MobileField(
-          onChanged: (mobile) {},
+          onChanged: (mobile) => _mobile = mobile,
         ),
         const SizedBox(height: 24),
         PINField(
-          onChanged: (pin) {},
+          onChanged: (pin) => _mPin = pin,
         ),
         const SizedBox(height: 24),
         PayButton(
-          amount: 1000,
-          onPressed: () {},
+          amount: config.amount,
+          onPressed: () async {
+            showProgressDialog(context, message: 'Initiating Payment');
+            try {
+              final response = await Khalti.service.initiatePayment(
+                request: PaymentInitiationRequestModel(
+                  mobile: _mobile!,
+                  transactionPin: _mPin!,
+                  amount: config.amount,
+                  productIdentity: config.productIdentity,
+                  productName: config.productName,
+                  productUrl: config.productUrl,
+                  additionalData: config.additionalData,
+                ),
+              );
+              Navigator.pop(context);
+              showSuccessDialog(
+                context,
+                title: 'Success',
+                subtitle:
+                    'Khalti has sent a confirmation code in your Khalti registered number.',
+                onPressed: () {
+                  print('done');
+                },
+              );
+            } catch (e) {
+              Navigator.pop(context);
+              showErrorDialog(
+                context,
+                error: e,
+                onPressed: () => Navigator.pop(context),
+              );
+            }
+          },
         ),
         const SizedBox(height: 40),
         Text(
@@ -41,6 +88,9 @@ class WalletPaymentPage extends StatelessWidget {
       ],
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
 
 class _ResetMPINSection extends StatelessWidget {
--- packages/khalti_flutter/lib/src/widget/dialogs.dart
+++ packages/khalti_flutter/lib/src/widget/dialogs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:khalti_flutter/src/helper/error_info.dart';
 import 'package:khalti_flutter/src/widget/image.dart';
 import 'package:khalti_flutter/src/widget/khalti_progress_indicator.dart';
+import 'package:url_launcher/url_launcher.dart' as launcher;
 
 Future<void> showProgressDialog(
   BuildContext context, {
@@ -48,63 +49,115 @@ Future<void> showErrorDialog(
     builder: (context) {
       final errorInfo = ErrorInfo.from(error);
 
-      return Dialog(
-        elevation: 0,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
-        insetPadding: const EdgeInsets.symmetric(horizontal: 16),
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            maxHeight: MediaQuery.of(context).size.height * 0.75,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(24, 32, 24, 24),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      KhaltiImage.asset(asset: 'dialog/error.svg', height: 72),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 24),
-                        child: Text(
-                          errorInfo.primary,
-                          style: Theme.of(context).textTheme.headline6,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      if (errorInfo.secondary != null)
-                        _ErrorBodyWidget(message: errorInfo.secondary!),
-                    ],
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Padding(
-                  padding: EdgeInsets.only(right: 8, bottom: 2),
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      primary: Color(0xFF5C2D91),
-                      textStyle: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    child: Text('OK'),
-                    onPressed: onPressed,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+      return _Dialog(
+        assetName: 'dialog/error.svg',
+        titleText: errorInfo.primary,
+        subtitle: errorInfo.secondary == null
+            ? null
+            : _ErrorBodyWidget(message: errorInfo.secondary!),
+        onPressed: onPressed,
       );
     },
   );
+}
+
+Future<void> showSuccessDialog(
+  BuildContext context, {
+  required String title,
+  required String subtitle,
+  required VoidCallback onPressed,
+}) {
+  return showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) {
+      return _Dialog(
+        assetName: 'dialog/success.svg',
+        titleText: title,
+        subtitle: Text(
+          subtitle,
+          style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                color: Color(0xFF474747),
+                height: 1.5,
+              ),
+        ),
+        onPressed: onPressed,
+      );
+    },
+  );
+}
+
+class _Dialog extends StatelessWidget {
+  const _Dialog({
+    Key? key,
+    required this.assetName,
+    required this.titleText,
+    required this.subtitle,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final String assetName;
+  final String titleText;
+  final Widget? subtitle;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      elevation: 0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+      insetPadding: const EdgeInsets.symmetric(horizontal: 16),
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxHeight: MediaQuery.of(context).size.height * 0.75,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(24, 32, 24, 24),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    KhaltiImage.asset(asset: assetName, height: 72),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 24),
+                      child: Text(
+                        titleText,
+                        style: Theme.of(context).textTheme.headline6,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    if (subtitle != null) subtitle!,
+                  ],
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: EdgeInsets.only(right: 8, bottom: 2),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Color(0xFF5C2D91),
+                    textStyle: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  child: Text('OK'),
+                  onPressed: onPressed,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class _ErrorBodyWidget extends StatelessWidget {
@@ -136,7 +189,7 @@ class _ErrorBodyWidget extends StatelessWidget {
               WidgetSpan(
                 child: InkWell(
                   onTap: () {
-                    print(link);
+                    if (link != null) launcher.launch(link);
                   },
                   child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 2),
@@ -153,7 +206,7 @@ class _ErrorBodyWidget extends StatelessWidget {
               ),
               if (trailingText != null) TextSpan(text: trailingText),
             ],
-            style: TextStyle(height: 1.5),
+            style: textStyle.copyWith(height: 1.5),
           ),
         );
       }
