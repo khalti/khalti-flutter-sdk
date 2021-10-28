640965493dbc3e8994716a3a1ae084a54fa2818e
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-28 19:26:12 +0545

    :bulb: added docs for `KhaltiClient`

--- packages/khalti_core/lib/src/core/http_client/khalti_client.dart
+++ packages/khalti_core/lib/src/core/http_client/khalti_client.dart
@@ -1,7 +1,16 @@
 import 'http_response.dart';
 
+/// The base http client to make request to Khalti server.
 abstract class KhaltiClient {
+  /// GET request
+  ///
+  /// [url] is the full URL for the API.
+  /// [params] is the query parameters to be sent with the request.
   Future<HttpResponse> get(String url, Map<String, Object> params);
 
+  /// POST request
+  ///
+  /// [url] is the full URL for the API.
+  /// [data] is the body to be sent with the request.
   Future<HttpResponse> post(String url, Map<String, Object?> data);
 }
