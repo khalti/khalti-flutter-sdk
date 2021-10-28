bd5ec5da353b5ab7a41d20d5a0c60be42af0f691
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-28 19:22:45 +0545

    :bulb: added docs for `HttpResponse`

--- packages/khalti_core/lib/src/config/url.dart
+++ packages/khalti_core/lib/src/config/url.dart
@@ -1,3 +1,8 @@
+/// Bank List API URL Path
 const String banks = 'bank/';
+
+/// Payment Initiation API URL Path
 const String initiateTransaction = 'payment/initiate/';
+
+/// Payment Confirmation API URL Path
 const String confirmTransaction = 'payment/confirm/';
--- packages/khalti_core/lib/src/core/http_client/http_response.dart
+++ packages/khalti_core/lib/src/core/http_client/http_response.dart
@@ -1,31 +1,43 @@
+import 'khalti_client.dart';
+
+/// The response for [KhaltiClient].
 class HttpResponse {
   const HttpResponse._({this.data, this.statusCode, this.message});
 
+  /// The [data] received.
   final Object? data;
+
+  /// The [statusCode] of response.
   final int? statusCode;
+
+  /// The error [message].
   final String? message;
 
+  /// Factory for [SuccessHttpResponse].
   factory HttpResponse.success({
     required Object data,
     required int statusCode,
-  }) = SuccessHttpResponse;
+  }) = SuccessHttpResponse._;
 
+  /// Factory for [FailureHttpResponse].
   factory HttpResponse.failure({
     required Object data,
     required int statusCode,
-  }) = FailureHttpResponse;
+  }) = FailureHttpResponse._;
 
+  /// Factory for [ExceptionHttpResponse].
   factory HttpResponse.exception({
     required String message,
     required int code,
     required StackTrace stackTrace,
     Object? detail,
     bool isSocketException,
-  }) = ExceptionHttpResponse;
+  }) = ExceptionHttpResponse._;
 }
 
+/// The success response for [KhaltiClient].
 class SuccessHttpResponse extends HttpResponse {
-  const SuccessHttpResponse({
+  const SuccessHttpResponse._({
     required Object data,
     required int statusCode,
   }) : super._(data: data, statusCode: statusCode);
@@ -36,8 +48,9 @@ class SuccessHttpResponse extends HttpResponse {
   }
 }
 
+/// The failure response for [KhaltiClient].
 class FailureHttpResponse extends HttpResponse {
-  const FailureHttpResponse({
+  const FailureHttpResponse._({
     required Object data,
     required int statusCode,
   }) : super._(data: data, statusCode: statusCode);
@@ -48,13 +61,21 @@ class FailureHttpResponse extends HttpResponse {
   }
 }
 
+/// The exception for [KhaltiClient].
 class ExceptionHttpResponse extends HttpResponse {
+  /// The error [code].
   final int code;
+
+  /// The [stackTrace] of the exception.
   final StackTrace stackTrace;
+
+  /// The exception detail
   final Object? detail;
+
+  /// Defines whether the exception is socket exception or not.
   final bool isSocketException;
 
-  const ExceptionHttpResponse({
+  const ExceptionHttpResponse._({
     required String message,
     required this.code,
     required this.stackTrace,
