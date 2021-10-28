e8cc98a3ee30662e8cb529e2c65e927171c916ad
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-28 16:05:31 +0545

    :memo: updated description and updated home page

---
+++ packages/khalti_flutter/CHANGELOG.md
@@ -0,0 +1,2 @@
+# [0.0.1] - Aug 27, 2021
+- No Changelog
--- packages/khalti_flutter/lib/localization/messages_en.dart
+++ packages/khalti_flutter/lib/localization/messages_en.dart
@@ -19,53 +19,87 @@ typedef String MessageIfAbsent(String? messageStr, List<Object>? args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
-  static m0(mobileNo) => "Enter the OTP sent via SMS to mobile number ${mobileNo}";
+  static m0(mobileNo) =>
+      "Enter the OTP sent via SMS to mobile number ${mobileNo}";
 
   static m1(method) => "Pay with ${method}";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function> {
-    "Amount" : MessageLookupByLibrary.simpleMessage("Amount"),
-    "An Error Occurred" : MessageLookupByLibrary.simpleMessage("An Error Occurred"),
-    "Cancel" : MessageLookupByLibrary.simpleMessage("Cancel"),
-    "Choose your payment method" : MessageLookupByLibrary.simpleMessage("Choose your payment method"),
-    "Confirm Payment" : MessageLookupByLibrary.simpleMessage("Confirm Payment"),
-    "Confirming Payment" : MessageLookupByLibrary.simpleMessage("Confirming Payment"),
-    "Connect IPS" : MessageLookupByLibrary.simpleMessage("Connect IPS"),
-    "E-Banking" : MessageLookupByLibrary.simpleMessage("E-Banking"),
-    "Forgot Khalti MPIN?" : MessageLookupByLibrary.simpleMessage("Forgot Khalti MPIN?"),
-    "Initiating Payment" : MessageLookupByLibrary.simpleMessage("Initiating Payment"),
-    "Install Khalti" : MessageLookupByLibrary.simpleMessage("Install Khalti"),
-    "Khalti" : MessageLookupByLibrary.simpleMessage("Khalti"),
-    "Khalti MPIN" : MessageLookupByLibrary.simpleMessage("Khalti MPIN"),
-    "Khalti MPIN must be at least 4 characters" : MessageLookupByLibrary.simpleMessage("Khalti MPIN must be at least 4 characters"),
-    "Khalti Mobile Number" : MessageLookupByLibrary.simpleMessage("Khalti Mobile Number"),
-    "Khalti has sent a confirmation code in your Khalti registered number and email address." : MessageLookupByLibrary.simpleMessage("Khalti has sent a confirmation code in your Khalti registered number and email address."),
-    "Khalti is not installed in your device. Either install Khalti App or proceed using your browser." : MessageLookupByLibrary.simpleMessage("Khalti is not installed in your device. Either install Khalti App or proceed using your browser."),
-    "Mobile Banking" : MessageLookupByLibrary.simpleMessage("Mobile Banking"),
-    "Network Unreachable" : MessageLookupByLibrary.simpleMessage("Network Unreachable"),
-    "No Connection" : MessageLookupByLibrary.simpleMessage("No Connection"),
-    "No Internet" : MessageLookupByLibrary.simpleMessage("No Internet"),
-    "No banks found" : MessageLookupByLibrary.simpleMessage("No banks found"),
-    "Ok" : MessageLookupByLibrary.simpleMessage("Ok"),
-    "Pay" : MessageLookupByLibrary.simpleMessage("Pay"),
-    "Payment Code" : MessageLookupByLibrary.simpleMessage("Payment Code"),
-    "Payment Code must be at least 6 characters" : MessageLookupByLibrary.simpleMessage("Payment Code must be at least 6 characters"),
-    "Please enter a valid mobile number" : MessageLookupByLibrary.simpleMessage("Please enter a valid mobile number"),
-    "Please search for another keyword" : MessageLookupByLibrary.simpleMessage("Please search for another keyword"),
-    "Please select your Bank" : MessageLookupByLibrary.simpleMessage("Please select your Bank"),
-    "Proceed using browser" : MessageLookupByLibrary.simpleMessage("Proceed using browser"),
-    "Reset Khalti MPIN" : MessageLookupByLibrary.simpleMessage("Reset Khalti MPIN"),
-    "SCT" : MessageLookupByLibrary.simpleMessage("SCT"),
-    "Search Bank" : MessageLookupByLibrary.simpleMessage("Search Bank"),
-    "Slow or no internet connection. Please check your internet & try again." : MessageLookupByLibrary.simpleMessage("Slow or no internet connection. Please check your internet & try again."),
-    "Success" : MessageLookupByLibrary.simpleMessage("Success"),
-    "Test" : MessageLookupByLibrary.simpleMessage("Test"),
-    "This field is required" : MessageLookupByLibrary.simpleMessage("This field is required"),
-    "Verify OTP" : MessageLookupByLibrary.simpleMessage("Verify OTP"),
-    "You are not connected to the internet. Please check your connection." : MessageLookupByLibrary.simpleMessage("You are not connected to the internet. Please check your connection."),
-    "Your connection could not be established." : MessageLookupByLibrary.simpleMessage("Your connection could not be established."),
-    "enterOtpSentTo" : m0,
-    "payWith" : m1
-  };
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "Amount": MessageLookupByLibrary.simpleMessage("Amount"),
+        "An Error Occurred":
+            MessageLookupByLibrary.simpleMessage("An Error Occurred"),
+        "Cancel": MessageLookupByLibrary.simpleMessage("Cancel"),
+        "Choose your payment method":
+            MessageLookupByLibrary.simpleMessage("Choose your payment method"),
+        "Confirm Payment":
+            MessageLookupByLibrary.simpleMessage("Confirm Payment"),
+        "Confirming Payment":
+            MessageLookupByLibrary.simpleMessage("Confirming Payment"),
+        "Connect IPS": MessageLookupByLibrary.simpleMessage("Connect IPS"),
+        "E-Banking": MessageLookupByLibrary.simpleMessage("E-Banking"),
+        "Forgot Khalti MPIN?":
+            MessageLookupByLibrary.simpleMessage("Forgot Khalti MPIN?"),
+        "Initiating Payment":
+            MessageLookupByLibrary.simpleMessage("Initiating Payment"),
+        "Install Khalti":
+            MessageLookupByLibrary.simpleMessage("Install Khalti"),
+        "Khalti": MessageLookupByLibrary.simpleMessage("Khalti"),
+        "Khalti MPIN": MessageLookupByLibrary.simpleMessage("Khalti MPIN"),
+        "Khalti MPIN must be at least 4 characters":
+            MessageLookupByLibrary.simpleMessage(
+                "Khalti MPIN must be at least 4 characters"),
+        "Khalti Mobile Number":
+            MessageLookupByLibrary.simpleMessage("Khalti Mobile Number"),
+        "Khalti has sent a confirmation code in your Khalti registered number and email address.":
+            MessageLookupByLibrary.simpleMessage(
+                "Khalti has sent a confirmation code in your Khalti registered number and email address."),
+        "Khalti is not installed in your device. Either install Khalti App or proceed using your browser.":
+            MessageLookupByLibrary.simpleMessage(
+                "Khalti is not installed in your device. Either install Khalti App or proceed using your browser."),
+        "Mobile Banking":
+            MessageLookupByLibrary.simpleMessage("Mobile Banking"),
+        "Network Unreachable":
+            MessageLookupByLibrary.simpleMessage("Network Unreachable"),
+        "No Connection": MessageLookupByLibrary.simpleMessage("No Connection"),
+        "No Internet": MessageLookupByLibrary.simpleMessage("No Internet"),
+        "No banks found":
+            MessageLookupByLibrary.simpleMessage("No banks found"),
+        "Ok": MessageLookupByLibrary.simpleMessage("Ok"),
+        "Pay": MessageLookupByLibrary.simpleMessage("Pay"),
+        "Payment Code": MessageLookupByLibrary.simpleMessage("Payment Code"),
+        "Payment Code must be at least 6 characters":
+            MessageLookupByLibrary.simpleMessage(
+                "Payment Code must be at least 6 characters"),
+        "Please enter a valid mobile number":
+            MessageLookupByLibrary.simpleMessage(
+                "Please enter a valid mobile number"),
+        "Please search for another keyword":
+            MessageLookupByLibrary.simpleMessage(
+                "Please search for another keyword"),
+        "Please select your Bank":
+            MessageLookupByLibrary.simpleMessage("Please select your Bank"),
+        "Proceed using browser":
+            MessageLookupByLibrary.simpleMessage("Proceed using browser"),
+        "Reset Khalti MPIN":
+            MessageLookupByLibrary.simpleMessage("Reset Khalti MPIN"),
+        "SCT": MessageLookupByLibrary.simpleMessage("SCT"),
+        "Search Bank": MessageLookupByLibrary.simpleMessage("Search Bank"),
+        "Slow or no internet connection. Please check your internet & try again.":
+            MessageLookupByLibrary.simpleMessage(
+                "Slow or no internet connection. Please check your internet & try again."),
+        "Success": MessageLookupByLibrary.simpleMessage("Success"),
+        "Test": MessageLookupByLibrary.simpleMessage("Test"),
+        "This field is required":
+            MessageLookupByLibrary.simpleMessage("This field is required"),
+        "Verify OTP": MessageLookupByLibrary.simpleMessage("Verify OTP"),
+        "You are not connected to the internet. Please check your connection.":
+            MessageLookupByLibrary.simpleMessage(
+                "You are not connected to the internet. Please check your connection."),
+        "Your connection could not be established.":
+            MessageLookupByLibrary.simpleMessage(
+                "Your connection could not be established."),
+        "enterOtpSentTo": m0,
+        "payWith": m1
+      };
 }
