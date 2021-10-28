1c2bdb2e344fc88bb586d159933245a14bf13a33
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-28 19:15:26 +0545

    :bulb: added docs for `KhaltiConfig`

--- packages/khalti_core/lib/src/config/khalti_config.dart
+++ packages/khalti_core/lib/src/config/khalti_config.dart
@@ -1,15 +1,29 @@
 import 'package:khalti_core/src/platform/platform.dart';
 
+/// The configuration class for Khalti Payment Gateway.
 class KhaltiConfig {
+  /// The version of the Khalti Payment Gateway Library.
   final String version = '0.0.1';
 
+  /// The device platform.
   final String platform;
+
+  /// The OS version.
   final String osVersion;
+
+  /// The device model.
   final String deviceModel;
+
+  /// The device manufacturer.
   final String deviceManufacturer;
+
+  /// The application package name.
   final String packageName;
+
+  /// Tha application package version.
   final String packageVersion;
 
+  /// Default constructor for the configuration.
   KhaltiConfig({
     required this.platform,
     required this.osVersion,
@@ -19,6 +33,8 @@ class KhaltiConfig {
     required this.packageVersion,
   });
 
+  /// A factory constructor that only configures the [platform]
+  /// and ignores everything else.
   factory KhaltiConfig.platformOnly() {
     return KhaltiConfig(
       platform: Platform.operatingSystem,
@@ -30,6 +46,7 @@ class KhaltiConfig {
     );
   }
 
+  /// The map representation of [KhaltiConfig].
   Map<String, String> get raw {
     return {
       'checkout-version': version,
