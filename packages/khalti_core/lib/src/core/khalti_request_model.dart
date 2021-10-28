f1a2b0ea26c722a1e0dadd1e294a002b7e74117d
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-28 19:28:13 +0545

    :bulb: added docs for `KhaltiRequestModel`

--- packages/khalti_core/lib/src/core/khalti_request_model.dart
+++ packages/khalti_core/lib/src/core/khalti_request_model.dart
@@ -1,8 +1,15 @@
 import 'dart:convert';
 
+import 'package:khalti_core/src/core/http_client/khalti_client.dart';
+
+/// The base request model for [KhaltiClient].
 abstract class KhaltiRequestModel {
+  /// The map representation of the [KhaltiRequestModel].
   Map<String, Object?> toMap();
 
+  /// The JSON representation of the [KhaltiRequestModel].
+  ///
+  /// Enabling [beautify] will prettify the JSON.
   String toJson({bool beautify = false}) {
     if (beautify) return const JsonEncoder.withIndent('  ').convert(toMap());
     return jsonEncode(toMap());
