81bff87348a808c278fe15b77f63b0f21eb96f04
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-29 09:48:39 +0545

    :bulb: added docs for [PaymentConfigScope]

--- packages/khalti_flutter/lib/src/helper/payment_config_provider.dart
+++ packages/khalti_flutter/lib/src/helper/payment_config_provider.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/widgets.dart';
-import 'package:khalti_flutter/src/helper/payment_config.dart';
 
+import 'payment_config.dart';
+
+/// The inherited provider of the [PaymentConfig].
 class PaymentConfigScope extends InheritedWidget {
+  /// Creates [PaymentConfigScope] from the provided values.
   const PaymentConfigScope({
     Key? key,
     required this.config,
     required Widget child,
   }) : super(key: key, child: child);
 
+  /// The [PaymentConfig] to be scoped in the widget tree.
   final PaymentConfig config;
 
+  /// Returns the [PaymentConfig] instance scoped in the widget tree.
   static PaymentConfig of(BuildContext context) {
     final PaymentConfigScope? configScope =
         context.dependOnInheritedWidgetOfExactType();
