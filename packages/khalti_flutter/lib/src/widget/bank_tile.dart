a1e9c7b7f292e6b8ed22173f7a6bbf3f653892d5
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-29 10:11:25 +0545

    :bulb: added docs for bank tile

--- packages/khalti_flutter/lib/src/widget/bank_tile.dart
+++ packages/khalti_flutter/lib/src/widget/bank_tile.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:khalti_flutter/src/widget/image.dart';
 
+/// The widget which shows bank's logo and name.
 class KhaltiBankTile extends StatelessWidget {
+  /// Creates [KhaltiBankTile] with the provided values.
   const KhaltiBankTile({
     Key? key,
     required this.logoUrl,
@@ -9,8 +11,13 @@ class KhaltiBankTile extends StatelessWidget {
     required this.onTap,
   }) : super(key: key);
 
+  /// The bank's logo url.
   final String logoUrl;
+
+  /// The bank's name.
   final String name;
+
+  /// Called when the user taps this tile.
   final VoidCallback onTap;
 
   @override
