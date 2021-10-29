2be7b8a7394ab4ad1ddb907132dc7034d7ff348a
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2021-10-29 10:52:03 +0545

    :bulb: added docs for `ResponsiveBox`

--- packages/khalti_flutter/lib/src/widget/responsive_box.dart
+++ packages/khalti_flutter/lib/src/widget/responsive_box.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
+/// The widget which constraints it's [child] depending upon the screen size.
 class ResponsiveBox extends StatelessWidget {
+  /// Creates [ResponsiveBox] with the provided properties.
   const ResponsiveBox({
     Key? key,
     required this.child,
     this.alignment = Alignment.center,
   }) : super(key: key);
 
+  /// The [child] widget.
   final Widget child;
+
+  /// How to align the child.
   final AlignmentGeometry alignment;
 
   @override
