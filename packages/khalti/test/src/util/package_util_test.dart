621eb4fdd9fc82d4b9edc28565f044f515e8c4eb
Biplab Dutta <biplabdutta860@gmail.com>
2022-07-27 16:56:53 +0545

    test(package_util): 100% test code coverage

---
+++ packages/khalti/test/src/util/package_util_test.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/services.dart';
+import 'package:khalti/src/util/package_util.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:package_info_plus_platform_interface/method_channel_package_info.dart';
+import 'package:package_info_plus_platform_interface/package_info_platform_interface.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:test/test.dart';
+
+class MockMethodChannelPackageInfo extends Mock
+    with MockPlatformInterfaceMixin
+    implements MethodChannelPackageInfo {}
+
+void main() {
+  late PackageUtil packageUtil;
+  late MockMethodChannelPackageInfo mockedMethodChannel;
+
+  setUp(
+    () {
+      packageUtil = PackageUtil();
+    },
+  );
+
+  void initialMockValues() => PackageInfo.setMockInitialValues(
+        appName: 'appName',
+        packageName: 'packageName',
+        version: 'version',
+        buildNumber: 'buildNumber',
+        buildSignature: 'buildSignature',
+      );
+
+  group(
+    'PackageUtil |',
+    () {
+      test(
+        'should throw PlatformException',
+        () async {
+          mockedMethodChannel = MockMethodChannelPackageInfo();
+          PackageInfoPlatform.instance = mockedMethodChannel;
+          when(() => mockedMethodChannel.getAll()).thenThrow(
+            PlatformException(code: ''),
+          );
+          await packageUtil.init();
+          verify(() => mockedMethodChannel.getAll()).called(1);
+        },
+      );
+
+      test(
+        'versionName: should return the version name',
+        () async {
+          initialMockValues();
+          await packageUtil.init();
+          final versionName = packageUtil.versionName;
+          expect(versionName, 'version');
+        },
+      );
+
+      test(
+        'applicationId: should return the application id',
+        () async {
+          initialMockValues();
+          await packageUtil.init();
+          final appId = packageUtil.applicationId;
+          expect(appId, 'packageName');
+        },
+      );
+
+      test(
+        'versionCode: should return the version code',
+        () async {
+          initialMockValues();
+          await packageUtil.init();
+          final versionCode = packageUtil.versionCode;
+          expect(versionCode, 'buildNumber');
+        },
+      );
+    },
+  );
+}
