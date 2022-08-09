4015e6b1b74bf4431e357a05c11126b27888d70f
Biplab Dutta <biplabdutta860@gmail.com>
2022-08-09 13:10:17 +0545

    Added tests for khalti package. (#23)
    * test: 100% test coverage
    
    * test(package_util): 100% test code coverage
    
    * add line breaks
    
    * test(package_util): 100% test code coverage
    
    * added tearDown
    
    * some edits
    
    * some edits
    
    * refactored test
    
    * changed launchUri to launch
    
    * edit descriptive message in test
    
    * test(khalti): 100% test coverage
    
    * made all the top-level elements private
    
    * made changes in accordance with received feedbacks
    
    * bumped versions
    
    * converted expression body to block body
    
    * added a line break
    
    * removed unnecessary setUp
    
    * renamed classes
    
    * update packages and used build context synchronously
    
    * made changes in accordance with received feedbacks
    
    * use single quote instead of double quote
    
    * move mocktail to dev_dependencies

---
+++ packages/khalti/test/khalti_test.dart
@@ -0,0 +1,68 @@
+import 'package:khalti/khalti.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const testPublicKey = '__test_public_key__';
+
+  final config = KhaltiConfig(
+    platform: 'Android',
+    osVersion: '12',
+    deviceModel: 'Pixel 6 Pro',
+    deviceManufacturer: 'Google',
+    packageName: 'com.khalti.test',
+    packageVersion: '3.0.0',
+  );
+
+  final expectedResult = <String, String>{
+    'checkout-version': '1.0.1',
+    'checkout-platform': 'linux',
+    'checkout-os-version': '',
+    'checkout-device-model': '',
+    'checkout-device-manufacturer': '',
+    'merchant-package-name': '',
+    'merchant-package-version': '',
+  };
+
+  group(
+    'Khalti: init() |',
+    () {
+      test(
+        'should return KhaltiConfig when parameter config is not null inside init method',
+        () async {
+          await Khalti.init(publicKey: testPublicKey, config: config);
+
+          expect(KhaltiService.config, config);
+        },
+      );
+
+      test(
+        'should initialize with default config if config is not provided during initialization',
+        () async {
+          await Khalti.init(
+            publicKey: testPublicKey,
+          );
+
+          expect(KhaltiService.config.raw, expectedResult);
+        },
+      );
+
+      test(
+        'service getter should return KhaltiService.',
+        () {
+          expect(Khalti.service, isA<KhaltiService>());
+        },
+      );
+
+      test(
+        'debugKhaltiServiceOverride setter should set a mocked KhaltiService',
+        () async {
+          Khalti.debugKhaltiServiceOverride = _KhaltiServiceMock();
+          expect(Khalti.service, isA<_KhaltiServiceMock>());
+        },
+      );
+    },
+  );
+}
+
+class _KhaltiServiceMock extends Mock implements KhaltiService {}
---
+++ packages/khalti/test/src/util/package_util_test.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/services.dart';
+import 'package:khalti/src/util/package_util.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:package_info_plus_platform_interface/method_channel_package_info.dart';
+import 'package:package_info_plus_platform_interface/package_info_platform_interface.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final packageUtil = PackageUtil();
+
+  group(
+    "package_util's init method",
+    () {
+      late _MethodChannelPackageInfoMock mockedMethodChannel;
+
+      setUp(() {
+        mockedMethodChannel = _MethodChannelPackageInfoMock();
+        PackageInfoPlatform.instance = mockedMethodChannel;
+      });
+
+      tearDown(
+        () {
+          PackageInfoPlatform.instance = MethodChannelPackageInfo();
+        },
+      );
+
+      test(
+        'should handle thrown PlatformException',
+        () async {
+          when(() => mockedMethodChannel.getAll()).thenThrow(
+            PlatformException(code: ''),
+          );
+          await packageUtil.init();
+          verify(() => mockedMethodChannel.getAll()).called(1);
+        },
+      );
+    },
+  );
+
+  group(
+    'PackageUtil |',
+    () {
+      setUp(
+        () async {
+          PackageInfo.setMockInitialValues(
+            appName: 'appName',
+            packageName: 'packageName',
+            version: 'version',
+            buildNumber: 'buildNumber',
+            buildSignature: 'buildSignature',
+          );
+          await packageUtil.init();
+        },
+      );
+
+      test(
+        'versionName: should return the version name',
+        () async {
+          final versionName = packageUtil.versionName;
+          expect(versionName, 'version');
+        },
+      );
+
+      test(
+        'applicationId: should return the application id',
+        () async {
+          final appId = packageUtil.applicationId;
+          expect(appId, 'packageName');
+        },
+      );
+
+      test(
+        'versionCode: should return the version code',
+        () async {
+          final versionCode = packageUtil.versionCode;
+          expect(versionCode, 'buildNumber');
+        },
+      );
+    },
+  );
+}
+
+class _MethodChannelPackageInfoMock extends Mock
+    with MockPlatformInterfaceMixin
+    implements MethodChannelPackageInfo {}
