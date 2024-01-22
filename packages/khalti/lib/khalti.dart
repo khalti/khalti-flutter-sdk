1484b0b8e56081c5b24b7b533f85ce126ffa951d
Biplab Dutta <biplabdutta860@gmail.com>
2024-01-22 10:56:38 +0545

    refactor: remove unnecessary code from `khalti.dart` file

--- packages/khalti/lib/khalti.dart
+++ packages/khalti/lib/khalti.dart
@@ -4,8 +4,6 @@ import 'package:flutter/foundation.dart';
 import 'package:khalti_core/khalti_core.dart';
 
 import 'src/khalti_http_client.dart';
-import 'src/util/device_util.dart';
-import 'src/util/package_util.dart';
 
 export 'package:khalti_core/khalti_core.dart';
 
@@ -19,36 +17,15 @@ abstract class Khalti {
   /// See the [Getting Started](https://docs.khalti.com/getting-started/),
   /// to find out about grabbing public key.
   ///
-  /// [config] describes the plugin configuration. By default some device and
-  /// package information are included alongside the platform type. Providing
-  /// custom config is not recommended.
-  ///
   /// [enabledDebugging] decides whether to show network logs or not.
   static Future<void> init({
     required String publicKey,
-    KhaltiConfig? config,
     bool enabledDebugging = false,
   }) async {
     KhaltiService.enableDebugging = enabledDebugging;
     KhaltiService.publicKey = publicKey;
 
-    if (config == null) {
-      final deviceUtil = DeviceUtil();
-      final packageUtil = PackageUtil();
-      await deviceUtil.init();
-      await packageUtil.init();
-
-      KhaltiService.config = KhaltiConfig(
-        platform: Platform.operatingSystem,
-        osVersion: deviceUtil.osVersion,
-        deviceModel: deviceUtil.deviceModel,
-        deviceManufacturer: deviceUtil.deviceManufacturer,
-        packageName: packageUtil.applicationId,
-        packageVersion: packageUtil.versionName,
-      );
-    } else {
-      KhaltiService.config = config;
-    }
+    await KhaltiConfig.getConfig();
   }
 
   /// A http [service] to make requests to Khalti APIs.
