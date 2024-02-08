b1036f5d2e7043487dbf4fa4959f7cd4450486e3
Biplab Dutta <biplabdutta860@gmail.com>
2024-02-08 15:34:05 +0545

    refactor: export `PaymentVerificationLookupResponseModel` from khalti package in khalti_flutter

--- packages/khalti_flutter/lib/khalti_flutter.dart
+++ packages/khalti_flutter/lib/khalti_flutter.dart
@@ -1,10 +1,7 @@
 // Copyright (c) 2021 The Khalti Authors. All rights reserved.
 
-export 'package:khalti/khalti.dart'
-    show PaymentSuccessModel, PaymentFailureModel;
+export 'package:khalti/khalti.dart' show PaymentVerificationLookupResponseModel;
 
 export 'localization/khalti_localizations.dart' show KhaltiLocalizations;
 export 'src/helper/payment_config.dart';
 export 'src/helper/payment_preference.dart';
-export 'src/widget/button.dart';
-export 'src/widget/khalti_scope.dart';
