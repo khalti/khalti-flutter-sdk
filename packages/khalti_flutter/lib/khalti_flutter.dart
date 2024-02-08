757122d836f41210b65d774b40604a827e24194e
Biplab Dutta <biplabdutta860@gmail.com>
2024-02-08 16:02:32 +0545

    remove unnecessary export

--- packages/khalti_flutter/lib/khalti_flutter.dart
+++ packages/khalti_flutter/lib/khalti_flutter.dart
@@ -1,7 +1,3 @@
 // Copyright (c) 2021 The Khalti Authors. All rights reserved.
 
 export 'package:khalti/khalti.dart' show PaymentVerificationLookupResponseModel;
-
-export 'localization/khalti_localizations.dart' show KhaltiLocalizations;
-export 'src/helper/payment_config.dart';
-export 'src/helper/payment_preference.dart';
