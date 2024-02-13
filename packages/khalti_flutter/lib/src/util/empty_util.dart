614a8cd2f91cec17cadc52e9d7167e8ad5947fb5
Biplab Dutta <biplabdutta860@gmail.com>
2024-02-13 16:46:09 +0545

    add empty util

---
+++ packages/khalti_flutter/lib/src/util/empty_util.dart
@@ -0,0 +1,22 @@
+/// Util containing various helper methods to deal with null checks.
+
+extension EmptyUtil on Object? {
+  /// Returns true is [Object] is null.
+  bool get isNull => this == null;
+
+  /// Returns true is [Object] is not null.
+  bool get isNotNull => this != null;
+
+  /// Returns true is [Object] is null or empty.
+  bool get isNullOrEmpty {
+    if (isNull) return true;
+    final object = this;
+    if (object is String && object.isEmpty) return true;
+    if (object is Iterable && object.isEmpty) return true;
+    if (object is Map && object.isEmpty) return true;
+    return false;
+  }
+
+  /// Returns true is [Object] is not null and not empty.
+  bool get isNotNullAndNotEmpty => !isNullOrEmpty;
+}
