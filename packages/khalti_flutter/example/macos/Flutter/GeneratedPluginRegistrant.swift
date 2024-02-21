eb845e8ef1f1c180b0e7257efafde7552a185c0d
Biplab Dutta <biplabdutta860@gmail.com>
2024-02-21 09:20:43 +0545

    make change to native iOS after adding flutter_inappwebview

--- packages/khalti_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/khalti_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import flutter_inappwebview_macos
 import package_info_plus
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
