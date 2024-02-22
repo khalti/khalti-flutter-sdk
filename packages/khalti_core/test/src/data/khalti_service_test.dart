45436c9d98638c83295115d3b062e4fc9f85aaae
Biplab Dutta <biplabdutta860@gmail.com>
2024-02-22 15:25:06 +0545

    test: improve test coverage for `khalti_service_test.dart`

--- packages/khalti_core/test/src/data/khalti_service_test.dart
+++ packages/khalti_core/test/src/data/khalti_service_test.dart
@@ -15,95 +15,102 @@ void main() {
     KhaltiService.publicKey = 'test-public-key';
   });
 
-  group('KhaltiService tests | ', () {
-    test(
-      'throws assertion error if public key was not set before accessing',
-      () {
-        KhaltiService.publicKey = '';
+  group(
+    'KhaltiService tests | ',
+    () {
+      test(
+        'throws assertion error if public key was not set before accessing',
+        () {
+          KhaltiService.publicKey = '';
 
-        expect(() => KhaltiService.publicKey, throwsA(isA<AssertionError>()));
-      },
-    );
-
-    test('confirmPayment() : success', () async {
-      mockClient.response = HttpResponse.success(
-        data: {
-          'token': 'test-token',
-          'merchant_image_url': 'https://cdn.khalti.com/image.png',
-          'product_name': 'Test Product',
-          'product_identity': 'test-product',
-          'product_url': 'https://khalti.com/bazaar/test-product',
-          'amount': 1000,
-          'mobile': '9810000000',
+          expect(() => KhaltiService.publicKey, throwsA(isA<AssertionError>()));
         },
-        statusCode: 200,
       );
-      final response = await service.confirmPayment(
-        request: PaymentConfirmationRequestModel(
-          transactionPin: '0000',
-          confirmationCode: '123456',
-          token: 'test-token',
-        ),
-      );
-      expect(response.token, 'test-token');
-      expect(
-        response.additionalData,
-        {'image_url': 'https://cdn.khalti.com/image.png'},
-      );
-      expect(response.productName, 'Test Product');
-      expect(response.productIdentity, 'test-product');
-      expect(response.productUrl, 'https://khalti.com/bazaar/test-product');
-      expect(response.amount, 1000);
-      expect(response.mobile, '9810000000');
-    });
-  });
 
-  group('KhaltiService log tests | ', () {
-    test(
-      'calling service methods logs the network calls if enableDebugging is true',
-      () {
-        final logs = <String>[];
-
-        Zone.current.fork(
-          specification: ZoneSpecification(
-            print: (_, __, ___, String msg) {
-              logs.add(msg);
+      test(
+        'verify() : success',
+        () async {
+          mockClient.response = HttpResponse.success(
+            data: {
+              "pidx": "eBjn5R4iUB82JnKCHjE7eG",
+              "total_amount": 1000,
+              "status": "Pending",
+              "transaction_id": 'Ax12NiopxWpalol7rI',
+              "fee": 0,
+              "refunded": false
             },
-          ),
-        ).run<void>(
-          () async {
-            KhaltiService.enableDebugging = true;
+            statusCode: 200,
+          );
+          final response = await service.verify('pidx-1234');
 
-            mockClient.response = HttpResponse.success(
-              data: {
-                'records': [
-                  {
-                    'name': 'NIC Asia Bank Limited',
-                    'short_name': 'NICA',
-                  },
-                  {
-                    'name': 'Himalayan Bank Limited',
-                    'short_name': 'HBL',
-                  },
-                ],
+          expect(
+            response,
+            isA<PaymentVerificationResponseModel>()
+                .having((model) => model.pidx, 'pidx', 'eBjn5R4iUB82JnKCHjE7eG')
+                .having((model) => model.totalAmount, 'Total Amount', 1000)
+                .having((model) => model.status, 'Status', 'Pending')
+                .having((model) => model.fee, 'Fee', 0)
+                .having((model) => model.refunded, 'Refunded', false)
+                .having(
+                  (model) => model.transactionId,
+                  'TransactionId',
+                  'Ax12NiopxWpalol7rI',
+                ),
+          );
+        },
+      );
+    },
+  );
+
+  group(
+    'KhaltiService log tests | ',
+    () {
+      test(
+        'calling service methods logs the network calls if enableDebugging is true',
+        () {
+          final logs = <String>[];
+
+          Zone.current.fork(
+            specification: ZoneSpecification(
+              print: (_, __, ___, String msg) {
+                logs.add(msg);
               },
-              statusCode: 200,
-            );
-            // await service.getBanks(
-            //   paymentType: PaymentType.eBanking,
-            // );
-            expect(logs[1], '[GET] https://khalti.com/api/v2/bank/');
-            expect(
-              logs[2],
-              '[Query Parameters] {page: 1, page_size: 200, payment_type: ebanking}',
-            );
-            expect(
-              logs[6],
-              '[Response] SuccessHttpResponse{data: {records: [{name: NIC Asia Bank Limited, short_name: NICA}, {name: Himalayan Bank Limited, short_name: HBL}]}, statusCode: 200}',
-            );
-          },
-        );
-      },
-    );
-  });
+            ),
+          ).run<void>(
+            () async {
+              KhaltiService.enableDebugging = true;
+
+              mockClient.response = HttpResponse.success(
+                data: {
+                  "pidx": "eBjn5R4iUB82JnKCHjE7eG",
+                  "total_amount": 1000,
+                  "status": "Pending",
+                  "transaction_id": 'Ax12NiopxWpalol7rI',
+                  "fee": 0,
+                  "refunded": false
+                },
+                statusCode: 200,
+              );
+
+              await service.verify('CMPrCaLxUFB8hiTcNmngr4');
+
+              expect(
+                logs.contains(
+                  '[POST] https://khalti.com/api/v2/epayment/lookup/',
+                ),
+                true,
+              );
+
+              expect(
+                logs.contains(
+                  '[Request Data] CMPrCaLxUFB8hiTcNmngr4',
+                ),
+                true,
+              );
+            },
+          );
+        },
+      );
+    },
+  );
 }
