d505efeb2cf6bf994ade5e514f1c7903715e1211
Biplab Dutta <biplabdutta860@gmail.com>
2024-03-07 14:11:45 +0545

    update example project

--- packages/khalti_flutter/example/lib/main.dart
+++ packages/khalti_flutter/example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:khalti_flutter/khalti_flutter.dart';
 
 void main() {
@@ -18,13 +17,19 @@ class KhaltiSDKDemo extends StatefulWidget {
 class _KhaltiSDKDemoState extends State<KhaltiSDKDemo> {
   late final Future<Khalti> khalti;
 
+  String pidx = 'X9akoo9aeE8ZNNZbjCQLxX';
+
+  PaymentResult? paymentResult;
+
   @override
   void initState() {
     super.initState();
     final payConfig = KhaltiPayConfig(
       publicKey: 'live_secret_key_68791341fdd94846a146f0457ff7b455',
-      pidx: 'uhn5Bne5iGWGbYib6iVhJ4',
-      returnUrl: Uri.parse('https://khalti.com'),
+      pidx: pidx,
+      returnUrl: Uri.parse(
+        'https://webhook.site/ed508278-3ce3-4f6d-98f1-0b6084c5c5cd',
+      ),
       environment: Environment.test,
     );
 
@@ -33,6 +38,9 @@ class _KhaltiSDKDemoState extends State<KhaltiSDKDemo> {
       payConfig: payConfig,
       onPaymentResult: (paymentResult) {
         log(paymentResult.toString());
+        setState(() {
+          this.paymentResult = paymentResult;
+        });
       },
       onMessage: ({description, statusCode}) async {
         log(
@@ -59,13 +67,46 @@ class _KhaltiSDKDemoState extends State<KhaltiSDKDemo> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SvgPicture.asset(
-                      'assets/khalti.svg',
-                      height: 50,
-                      width: 50,
+                    Image.asset(
+                      'assets/seru.png',
+                      height: 200,
+                      width: 200,
                     ),
-                    const SizedBox(height: 16),
-                    const Text('Pay with Khalti')
+                    const SizedBox(height: 120),
+                    const Text(
+                      'Rs. 22',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    const Text('1 day fee'),
+                    OutlinedButton(
+                      onPressed: () => khalti.start(context),
+                      child: const Text('Pay with Khalti'),
+                    ),
+                    const SizedBox(height: 120),
+                    paymentResult == null
+                        ? Text(
+                            'pidx: $pidx',
+                            style: const TextStyle(fontSize: 15),
+                          )
+                        : Column(
+                            children: [
+                              Text(
+                                'pidx: ${paymentResult!.payload?.pidx}',
+                              ),
+                              Text('Status: ${paymentResult!.status}'),
+                              Text(
+                                'Amount Paid: ${paymentResult!.payload?.amount}',
+                              ),
+                              Text(
+                                'Transaction ID: ${paymentResult!.payload?.transactionId}',
+                              ),
+                            ],
+                          ),
+                    const SizedBox(height: 120),
+                    const Text(
+                      'This is a demo application developed by some merchant.',
+                      style: TextStyle(fontSize: 12),
+                    )
                   ],
                 ),
               ),
