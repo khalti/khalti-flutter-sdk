8f3f22d5ff58f32dca87da8038b853ce38c8ed16
Biplab Dutta <biplabdutta860@gmail.com>
2024-03-07 14:17:47 +0545

    add all the hardcoded strings in `strings.dart`

---
+++ packages/khalti_flutter/lib/src/strings.dart
@@ -0,0 +1,11 @@
+/// Pay with Khalti
+const s_payWithKhalti = 'Pay with Khalti';
+
+/// No internet
+const s_noInternet = 'No internet';
+
+/// Please connect to an internet and proceed.
+const s_noInternetDisplayMessage = 'Please connect to an internet to proceed.';
+
+/// Go back
+const s_tryAgain = 'Try Again';
