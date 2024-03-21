56a86782850eccc2a464e43345d437cff02ebdc8
Biplab Dutta <biplabdutta860@gmail.com>
2024-03-21 14:49:21 +0545

    update example app

--- packages/khalti_flutter/example/lib/main.dart
+++ packages/khalti_flutter/example/lib/main.dart
@@ -21,7 +21,8 @@ class KhaltiSDKDemo extends StatefulWidget {
 class _KhaltiSDKDemoState extends State<KhaltiSDKDemo> {
   late final Future<Khalti?> khalti;
 
-  String pidx = 'yr4vYk3dTXhe6Lc3TSzxyg';
+  String pidx =
+      'GM8tsmvxwQvZq2soX3V4BW'; // Should be generated via a server-side POST request.
 
   PaymentResult? paymentResult;
 
@@ -31,9 +32,7 @@ class _KhaltiSDKDemoState extends State<KhaltiSDKDemo> {
     final payConfig = KhaltiPayConfig(
       publicKey: 'live_public_key_979320ffda734d8e9f7758ac39ec775f',
       pidx: pidx,
-      returnUrl: Uri.parse(
-        'https://webhook.site/ed508278-3ce3-4f6d-98f1-0b6084c5c5cd',
-      ),
+      returnUrl: Uri.parse('https://docs.khalti.com/khalti-epayment/'),
       environment: Environment.test,
     );
 
@@ -74,53 +73,50 @@ class _KhaltiSDKDemoState extends State<KhaltiSDKDemo> {
             if (khaltiSnapshot == null) {
               return const CircularProgressIndicator.adaptive();
             }
-            return GestureDetector(
-              onTap: () => khaltiSnapshot.open(context),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/seru.png',
-                    height: 200,
-                    width: 200,
-                  ),
-                  const SizedBox(height: 120),
-                  const Text(
-                    'Rs. 22',
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  const Text('1 day fee'),
-                  OutlinedButton(
-                    onPressed: () => khaltiSnapshot.open(context),
-                    child: const Text('Pay with Khalti'),
-                  ),
-                  const SizedBox(height: 120),
-                  paymentResult == null
-                      ? Text(
-                          'pidx: $pidx',
-                          style: const TextStyle(fontSize: 15),
-                        )
-                      : Column(
-                          children: [
-                            Text(
-                              'pidx: ${paymentResult!.payload?.pidx}',
-                            ),
-                            Text('Status: ${paymentResult!.status}'),
-                            Text(
-                              'Amount Paid: ${paymentResult!.payload?.amount}',
-                            ),
-                            Text(
-                              'Transaction ID: ${paymentResult!.payload?.transactionId}',
-                            ),
-                          ],
-                        ),
-                  const SizedBox(height: 120),
-                  const Text(
-                    'This is a demo application developed by some merchant.',
-                    style: TextStyle(fontSize: 12),
-                  )
-                ],
-              ),
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/seru.png',
+                  height: 200,
+                  width: 200,
+                ),
+                const SizedBox(height: 120),
+                const Text(
+                  'Rs. 22',
+                  style: TextStyle(fontSize: 25),
+                ),
+                const Text('1 day fee'),
+                OutlinedButton(
+                  onPressed: () => khaltiSnapshot.open(context),
+                  child: const Text('Pay with Khalti'),
+                ),
+                const SizedBox(height: 120),
+                paymentResult == null
+                    ? Text(
+                        'pidx: $pidx',
+                        style: const TextStyle(fontSize: 15),
+                      )
+                    : Column(
+                        children: [
+                          Text(
+                            'pidx: ${paymentResult!.payload?.pidx}',
+                          ),
+                          Text('Status: ${paymentResult!.status}'),
+                          Text(
+                            'Amount Paid: ${paymentResult!.payload?.amount}',
+                          ),
+                          Text(
+                            'Transaction ID: ${paymentResult!.payload?.transactionId}',
+                          ),
+                        ],
+                      ),
+                const SizedBox(height: 120),
+                const Text(
+                  'This is a demo application developed by some merchant.',
+                  style: TextStyle(fontSize: 12),
+                )
+              ],
             );
           },
         ),
